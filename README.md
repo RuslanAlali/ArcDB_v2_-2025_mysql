a4a84e1d06ca444db4fd97d92742a629bcf63292
RuslanAlali <ruslan.alali@gmail.com>
2025-09-02 11:53:07 +0200

    final fix for 2.0

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mysql_db_functioning
+## Plan
+Import all variants from VCF files and all metadata about the experimetns and non-critical information about patients. 
+
+ It saves the database in the folder /mnt/ruslan/ArcDB_v2/mysql_db/datadir on Hermes server. Copies are saved on /internal-bioinfo/ruslan/small_projects/ArcDB/datadir{date}.
+The code is saved on github https://github.com/RuslanAlali/ArcDB_v2_-2025_mysql.
+
+## Characteristics of the project
+The database technology is mySQL V8.0.42. The codes are written using python3 (on vscode). 
--- run_ones_codes/validate_ArcDB.py
+++ run_ones_codes/validate_ArcDB.py
@@ -7,12 +7,12 @@ ArcDB2_hq=pd.read_csv('merged_variant_data_hq.csv')
 sample_DB2=3271
 
 ArcDB1=ArcDB1.loc[ArcDB1['count']>(4861*5//100)]
-ArcDB2=ArcDB2.loc[ArcDB2['count']>(sample_DB2*4//100)]
+ArcDB2=ArcDB2.loc[ArcDB2['count']>(sample_DB2*5//100)]
 ArcDB2_hq=ArcDB2_hq.loc[ArcDB2_hq['count']>(sample_DB2*5//100)]
 
-ArcDB1['variant']=ArcDB1['CHROM']+ArcDB1['POS'].astype('string')+ArcDB1['REF']+"_"+ArcDB1['ALT']
-ArcDB2['variant']=ArcDB2['Chrom']+ArcDB2['Pos'].astype('string')+ArcDB2['Ref']+"_"+ArcDB2['Alt']
-ArcDB2_hq['variant']=ArcDB2_hq['Chrom']+ArcDB2_hq['Pos'].astype('string')+ArcDB2_hq['Ref']+"_"+ArcDB2_hq['Alt']
+ArcDB1['variant']=ArcDB1['CHROM']+":"+ArcDB1['POS'].astype('string')+"_"+ArcDB1['REF']+"_"+ArcDB1['ALT']
+ArcDB2['variant']=ArcDB2['Chrom']+":"+ArcDB2['Pos'].astype('string')+"_"+ArcDB2['Ref']+"_"+ArcDB2['Alt']
+ArcDB2_hq['variant']=ArcDB2_hq['Chrom']+":"+ArcDB2_hq['Pos'].astype('string')+"_"+ArcDB2_hq['Ref']+"_"+ArcDB2_hq['Alt']
 
 x=ArcDB1['variant']
 set1 = set(x[x.str.len()<150])
@@ -30,6 +30,8 @@ print(f"Common items: {len(common_items)} ")
 print(f"Unique in ArcDB1: {len(unique_in_col1)} ")
 print(f"Unique in ArcDB2: {len(unique_in_col2)} ")
 
+ArcDB1x = ArcDB1[ArcDB1['variant'].isin(unique_in_col1)]
+ArcDB1x.to_excel('unique_ArcDB1.xlsx',index=False)
 
 common_items = set1 & set2hq
 unique_in_col1 = set1 - common_items
